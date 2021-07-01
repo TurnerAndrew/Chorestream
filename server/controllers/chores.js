3dbf2758c424a9e2c2e8fdf9d0b5c0562b96dcc5
Andrew Turner <turner.andrew0@gmail.com>
2021-07-01 09:24:42 -0500

    Added getChores function

--- server/controllers/chores.js
+++ server/controllers/chores.js
@@ -6,11 +6,17 @@ module.exports = {
         const { user_id } = req.session.user
 
         const [chore] = await db.chores.addChore([task, min_age, max_age, difficulty, user_id, frequency])
+        
+    },
 
+    getChores: async (req, res) => {
+        const db = req.app.get('db')
+        const {household} = req.body
+        const chores = await db.chores.getChores([household])
+
+        res.status(200).send(chores)
 
     }
-
-
 
 
 }
